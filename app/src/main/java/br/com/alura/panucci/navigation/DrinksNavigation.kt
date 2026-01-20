ccc50fbb0ec12ee78ec292ded12efd6c02ab08ab
Isabela Fonseca <isabela.fonseca@fulltime.com.br>
2026-01-20 11:07:24 -0300

    Separating navigation graphics Configuring destination navigation Integrating visual components Standardizing navigation as Type safety Modifying navigation options

---
+++ app/src/main/java/br/com/alura/panucci/navigation/DrinksNavigation.kt
@@ -0,0 +1,28 @@
+package br.com.alura.panucci.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import br.com.alura.panucci.sampledata.sampleProducts
+import br.com.alura.panucci.ui.screens.DrinksListScreen
+
+internal const val drinksRoute = "drinks"
+
+fun NavGraphBuilder.drinksScreen(navController: NavHostController) {
+    composable(drinksRoute) {
+        DrinksListScreen(
+            products = sampleProducts,
+            onNavigateToDetails = { product ->
+                navController.navigateToProductDetails(product.id)
+            },
+        )
+    }
+}
+
+fun NavController.navigateToDrinks(
+    navOptions: NavOptions? = null
+) {
+    navigate(drinksRoute, navOptions)
+}
---
+++ app/src/main/java/br/com/alura/panucci/navigation/HighlightsListNavigation.kt
@@ -0,0 +1,31 @@
+package br.com.alura.panucci.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import br.com.alura.panucci.sampledata.sampleProducts
+import br.com.alura.panucci.ui.screens.HighlightsListScreen
+
+internal const val highlightListRoute = "highlight"
+
+fun NavGraphBuilder.highlightGraphListScreen(navController: NavHostController) {
+    composable(highlightListRoute) {
+        HighlightsListScreen(
+            products = sampleProducts,
+            onNavigateToDetails = { product ->
+                navController.navigateToProductDetails(product.id)
+            },
+            onNavigateToCheckout = {
+                navController.navigateToCheckout()
+            },
+        )
+    }
+}
+
+fun NavController.navigateToHighlight(
+    navOptions: NavOptions? = null
+) {
+    navigate(highlightListRoute, navOptions)
+}
